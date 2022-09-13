3ec620b253415490f5baa3b7aa4752a73c71eefc
juan-sk <juan.ramirez.sk@gmail.com>
2022-09-13 16:27:18 -0400

    avances en mostrar componentes en el mapa

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  markers: any = [{}]
+  @Input() markers: any;
+  showMap: boolean = true
   zoom = 18;
   center: google.maps.LatLngLiteral = {
     lat: 0,
@@ -28,20 +29,21 @@ export class MapComponent implements OnInit {
   });
 
   ngOnInit() {
+    console.log({ markers: this.markers })
     navigator.geolocation.getCurrentPosition((position) => {
       this.center = {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       };
-      // this.creaPath()
       console.log(this.center)
     });
-    setInterval(() => {
-      if (this.markers.length > 0) {
+    // let interval = setInterval(() => {
+    //   if (this.markers != undefined) {
+    //     console.log(this.markers)
+    //     this.showMap = true;
+    //   }
+    // }, 10)
 
-        this.updateMap()
-      }
-    }, 500)
   }
   creaPath(coords: any) {
 
@@ -58,16 +60,15 @@ export class MapComponent implements OnInit {
 
     if (this.options != undefined) {
 
-      // if (this.zoom < this.options.maxZoom) {
+
       this.zoom++;
-      // }
+
     }
   }
 
   zoomOut() {
-    // if (this.zoom > this.options.minZoom) {
     this.zoom--;
-    // }
+
   }
 
 
@@ -81,64 +82,10 @@ export class MapComponent implements OnInit {
     console.log({ lat: event.latLng.lat(), lng: event.latLng.lng() })
     let coords = { lat: event.latLng.lat(), lng: event.latLng.lng() }
     this.creaPath(coords)
-    // this.markers.push({
-    //   position: {
-    //     lat: this.center.lat + ((Math.random() - 0.5) * 2) / 10,
-    //     lng: this.center.lng + ((Math.random() - 0.5) * 2) / 10,
-    //   },
-    //   label: {
-    //     color: 'red',
-    //     text: 'Marker label ' + (this.markers.length + 1),
-    //   },
-    //   title: 'Marker title ' + (this.markers.length + 1),
-
-    //   options: {
-    //     // animation: google.maps.Animation.DROP,
-    //     icon: '../assets/icons/colectivo.png'
-    //   },
-    // });
-  }
-
-  updateMap() {
-    let colectivosupdateados = this.getColectivos();
-    for (let i = 0; i < this.markers.length; i++) {
-      let marker = this.markers[i];
-      marker = colectivosupdateados[i]
-    }
-    this.markers = colectivosupdateados;
-
 
   }
 
-  offset: number = 1;
-  getColectivos() {
-    let listaBase = [
-      {
-        position: { lat: -33.51583, lng: -70.5673618 },
-        label: {
-          color: 'red',
-          text: 'Marker label ' + (this.markers.length + 1),
-        },
-        title: 'Marker title ' + (this.markers.length + 1),
 
-        options: {
-          // animation: google.maps.Animation.DROP,
-          icon: '../assets/icons/colectivo.png'
-        },
-      }
-    ]
-
-    let constante = 0.00001
-    let newLIst = listaBase.map((e) => {
-
-      e.position.lng = e.position.lng - (constante * this.offset);
-      return e
-    })
-    this.offset++;
-
-
-    return newLIst;
-  }
 
   // @HostListener('window:scroll', ['$event']) // for window scroll events
   onScroll(event: any) {
--- src/app/components/pasajero/mapa-pasajero/mapa-pasajero.component.ts
+++ src/app/components/pasajero/mapa-pasajero/mapa-pasajero.component.ts
@@ -8,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 export class MapaPasajeroComponent implements OnInit {
 
   constructor() { }
-
+  markers: any = [{}]
+  showMap: boolean = false;
   ngOnInit(): void {
+    setInterval(() => {
+      if (this.markers.length > 0) {
+
+        this.updateMap()
+        this.showMap = true;
+      }
+    }, 500)
   }
 
   height = window.screen.height;
+
+  offset: number = 1;
+  getColectivos() {
+    let listaBase = [
+      {
+        position: { lat: -33.51583, lng: -70.5673618 },
+        label: {
+          color: 'red',
+          text: 'Marker label ' + (this.markers.length + 1),
+        },
+        title: 'Marker title ' + (this.markers.length + 1),
+
+        options: {
+          // animation: google.maps.Animation.DROP,
+          icon: '../assets/icons/colectivo.png'
+        },
+      }
+    ]
+
+    let constante = 0.00001
+    let newLIst = listaBase.map((e) => {
+
+      e.position.lng = e.position.lng - (constante * this.offset);
+      return e
+    })
+    this.offset++;
+
+
+    return newLIst;
+  }
+
+
+  updateMap() {
+    let colectivosupdateados = this.getColectivos();
+    for (let i = 0; i < this.markers.length; i++) {
+      let marker = this.markers[i];
+      marker = colectivosupdateados[i]
+    }
+    this.markers = colectivosupdateados;
+
+
+  }
 }
